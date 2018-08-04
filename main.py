a48e42c0452fe88007480afb9daf57e12e74143d
glapa-grossklag <m.glapa.grossklag@gmail.com>
2018-08-04 14:37:28 -0700

    Added a check to ensure that the log dir exists, removed unused imports

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
-import shutil
-import discord
 import argparse
 from datetime import datetime
+import discord
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -13,31 +12,35 @@ parser.add_argument("password", help = "Discord password to login with")
 parser.add_argument("--flushlogs", help = "clear all previously stored chat logs", action = "store_true")
 args = parser.parse_args()
 
+# Create logs folder if it doesn't exist
+if not os.path.exists("./logs/"):
+    print("Creating log folder...")
+    os.makedirs("./logs/")
+
 # Wipe all log files
 if args.flushlogs:
     for log in os.listdir("./logs/"):
         os.unlink("./logs/" + log)
     print("Cleared all chat logs")
 
-# Discord listeners
 client = discord.Client()
+print("Connecing to Discord...")
 
 @client.event
 async def on_ready():
-    print(f"Connected as {client.user.name}")
+    print(f"Logged in as {client.user.name}")
 
 @client.event
 async def on_message(message):
-    user = message.author
-    if message.channel.is_private:
-        id_to_use = ""
-        if user == client.user:
-            id_to_use = message.channel.recipients[0].id
-        else:
-            id_to_use = message.author.id
+    if not message.channel.is_private: return
 
-        with open("./logs/" + id_to_use + ".log", "a") as file:
-            file.write(user.name+ "#" + user.discriminator + " at " + datetime.now().isoformat() + "\n")
-            file.write(message.content + "\n" * 2)
+    if message.author == client.user:
+        id = message.channel.recipients[0].id
+    else:
+        id  = message.author.id
+
+    with open("./logs/" + id + ".log", "a") as file:
+        file.write(message.author.name+ "#" + message.author.discriminator + " at " + datetime.now().isoformat() + "\n")
+        file.write(message.content + "\n" * 2)
 
 client.run(args.email, args.password)

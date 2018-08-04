1b0e154cd9ec9a35178f547116d604bbdcb4f610
glapa-grossklag <m.glapa.grossklag@gmail.com>
2018-08-04 15:04:31 -0700

    Moved logging to a seperate function

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ parser.add_argument("password", help = "Discord password to login with")
 parser.add_argument("--flushlogs", help = "clear all previously stored chat logs", action = "store_true")
 args = parser.parse_args()
 
+def log(message, text):
+    # Select ID to use in logfile name
+    filename = message.channel.recipients[0].id if (message.author == client.user) else message.author.id
+    with open("./logs/" + filename + ".log", "a") as file:
+        file.write(message.author.name+ "#" + message.author.discriminator + " at " + datetime.now().isoformat() + "\n")
+        file.write(text + "\n" * 2)
+
 # Create logs folder if it doesn't exist
 if not os.path.exists("./logs/"):
     print("Creating log folder...")
@@ -33,14 +40,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if not message.channel.is_private: return
-
-    if message.author == client.user:
-        id = message.channel.recipients[0].id
-    else:
-        id  = message.author.id
-
-    with open("./logs/" + id + ".log", "a") as file:
-        file.write(message.author.name+ "#" + message.author.discriminator + " at " + datetime.now().isoformat() + "\n")
-        file.write(message.content + "\n" * 2)
+    log(message, message.content)
 
 client.run(args.email, args.password)
